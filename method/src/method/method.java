74672e486a4d6f26c5f79631985470b24488de7d
ACER <ACER@DESKTOP-P4DU9JL>
2021-02-18 11:21:42 +0300

    1

---
+++ method/src/method/method.java
@@ -0,0 +1,37 @@
+
+import java.utill.Scanner;
+
+public class method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter two numbers int between + or - or / or *)
+		scanner input = new scanner(System.in);
+		 int a = input.nextint();
+		 int b = input.nextint();
+		 int c = input.nextint();
+		 int d = input.nextint();
+		 int e = input.nextint();
+		 int f = input.nextint();
+		 int j = input.nextint();
+		 int h = input.nextint();
+}
+         public static sum (int a , int b){
+        	 system.out.println("a+b");
+        	 return a+b; }
+         public static subtract (int c , int d){
+        	 system.out.println("c-d");
+        	 return c-d;
+        	 }
+         public static diving (int e , int f){
+        	 system.out.println("e/f");
+         }
+         public static hit (int j , int h){
+        	 system.out.println("j*h");
+         }
+        	  
+         }
+        	 
+        	 
+        	 
+         }
